47976533fd98ac01ca6db74e8319c0de7c88f5e7
David Pastor <helmutp@thehangar.cr>
2020-03-15 21:15:24 -0600

    added base route and info

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,50 @@ const express = require("express");
 const router = express.Router();
 const controllers = require("../controllers/Covid");
 
+router.get("/", function(req, res, next) {
+  res.json({
+    api: "OK",
+    endpoints: {
+      confirmed: {
+        GET: "/confirmed",
+        POST: {
+          route: "/confirmed",
+          body: {
+            country: "YOUR COUNTRY"
+          }
+        }
+      },
+      recovered: {
+        GET: "/recovered",
+        POST: {
+          route: "/recovered",
+          body: {
+            country: "YOUR COUNTRY"
+          }
+        }
+      },
+      deaths: {
+        GET: "/deaths",
+        POST: {
+          route: "/deaths",
+          body: {
+            country: "YOUR COUNTRY"
+          }
+        }
+      },
+      latest: {
+        GET: "/latest",
+        POST: {
+          route: "/latest",
+          body: {
+            country: "YOUR COUNTRY"
+          }
+        }
+      }
+    }
+  });
+});
+
 /* GET Confirmed. */
 router.get("/confirmed", controllers.getConfirmed);
 router.post("/confirmed", controllers.getConfirmedByCountry);
